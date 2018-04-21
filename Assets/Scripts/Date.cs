31994ec7d7b56a9ec7b3145b8a569249206b398e
Trenton Pottruff <jayitinc@gmail.com>
2018-04-20 22:54:16 -0400

    Init

---
+++ Assets/Scripts/Date.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Date : MonoBehaviour {
+    public TurnPhase turnPhase = TurnPhase.PlayerChoice;
+    public GameObject choiceButtons;
+
+    private void Update() {
+        switch(turnPhase) {
+            case TurnPhase.PlayerChoice:
+                PlayerChoicePhase();
+                break;
+            case TurnPhase.PlayerAttack:
+                PlayerAttackPhase();
+                break;
+            case TurnPhase.EnemyDialog:
+                EnemyDialogPhase();
+                break;
+            case TurnPhase.EnemyAttack:
+                EnemyAttackPhase();
+                break;
+        }
+    }
+
+    private void PlayerChoicePhase() {
+        choiceButtons.SetActive(true);
+    }
+
+    private void PlayerAttackPhase() {
+
+    }
+
+    private void EnemyDialogPhase() {
+
+    }
+
+    private void EnemyAttackPhase() {
+        
+    }
+
+    public void ChoiceFlirt() {
+
+    }
+
+    public void ChoiceJoke() {
+
+    }
+
+    public void ChoiceStory() {
+
+    }
+}
+
+public enum TurnPhase {
+    PlayerChoice, PlayerAttack, EnemyDialog, EnemyAttack
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    public bool canMove = true;
+    public float speed = 5f;
+
+    private Vector2 inputVector;
+    private Vector2 directionVector;
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        directionVector = inputVector.normalized;
+
+        if (canMove) {
+            rb.velocity = directionVector * speed;
+        }
+    }
+}
